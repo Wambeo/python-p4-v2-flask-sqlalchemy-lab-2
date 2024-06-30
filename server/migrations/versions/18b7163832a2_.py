d5583a66733cc034093d3ab2020cb0ca948aa99d
Samuel Wambeo <samuelwambeo9@gmail.com>
2024-06-30 09:41:37 +0300

    complete

---
+++ server/migrations/versions/18b7163832a2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 18b7163832a2
+Revises: 35608779f885
+Create Date: 2024-06-30 09:22:22.058758
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18b7163832a2'
+down_revision = '35608779f885'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
